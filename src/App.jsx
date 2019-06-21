6b56216ea7d91e98eae25b23fe2783eaf0edf7ae
MatthewYiHe <matthewhe1@hotmail.com>
2019-06-21 20:46:01 +0000

    have the plus15 map in the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
+import SimpleMap from './SimpleMap.jsx';
 
 class App extends Component {
   render() {
     return (
+      <section>
       <h1>Hello React :)</h1>
+      <SimpleMap />
+      </section>
+
+
     );
   }
 }
