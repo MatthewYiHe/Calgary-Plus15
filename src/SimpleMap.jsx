ee02d0c85badb95c55d640d43f83a7b70a40eeb0
MatthewYiHe <matthewhe1@hotmail.com>
2019-06-23 06:49:28 +0000

    add marker and sample polyline

--- src/SimpleMap.jsx
+++ src/SimpleMap.jsx
@@ -4,6 +4,21 @@ import GoogleMapReact from 'google-map-react';
 const AnyReactComponent = ({ marker }) => <div>{marker}</div>;
 
 class SimpleMap extends Component {
+  constructor() {
+    super();
+    this.state = {
+                  markers: [
+                    {
+                      name: "Current position",
+                      position: {
+                        lat: 51.05,
+                        lng: -114.06
+                      }
+                    }
+                  ]
+    };
+  }
+
   static defaultProps = {
     center: {
       lat: 51.044921,
@@ -12,20 +27,36 @@ class SimpleMap extends Component {
     zoom: 15
   };
 
-  renderMarkers(map, maps) {
+  renderMarkers = (map, maps) => {
     let marker = new maps.Marker({
       map:map,
       draggable:true,
-      animation: google.maps.Animation.DROP,
-      position: {lat: 51.044921, lng: -114.064691},
+      position: {lat: this.state.markers[0].position.lat, lng: this.state.markers[0].position.lng},
       title:"Drag me!"
     });
   }
 
+  renderPolylines (map, maps) {
+    // let position = marker.getPosition()
+    // let lat = position.lat()
+    // let lng = position.lng()
+    let geodesicPolyline = new maps.Polyline({
+      path: [
+        {lat:51.05, lng:-114.06},
+        {lat:51.045, lng:-114.062}
+      ],
+      strokeColor: 'red',
+      strokeOpacity: 1,
+      strokeWeight: 5
+    })
+    geodesicPolyline.setMap(map)
+  }
+
+
+
   render() {
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '80vh', width: '50%'}}>
+      <div style={{ height: '80vh', width: '100%'}}>
         <GoogleMapReact
           bootstrapURLKeys={{ key:"AIzaSyBFruyJsEnujgDzqV9HybXjA6ySEahuwJU"}}
           defaultCenter={this.props.center}
@@ -34,8 +65,11 @@ class SimpleMap extends Component {
           onGoogleApiLoaded={({map, maps}) => {
             map.data.loadGeoJson('./Plus15.geojson')
             this.renderMarkers(map, maps)
+            this.renderPolylines(map, maps)
           }}
         >
+
+
           <AnyReactComponent
             lat={51.044921}
             lng={-114.064691}
@@ -47,4 +81,7 @@ class SimpleMap extends Component {
   }
 }
 
+
+
 export default SimpleMap;
+
