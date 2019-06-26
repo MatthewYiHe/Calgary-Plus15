306258953efc31ee4316d7d4223c3e094e907981
MatthewYiHe <matthewhe1@hotmail.com>
2019-06-26 18:07:44 +0000

    Revert "have the plus15 map in the page"
    This reverts commit 6b56216ea7d91e98eae25b23fe2783eaf0edf7ae.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React, {Component} from 'react';
-import SimpleMap from './SimpleMap.jsx';
 
 class App extends Component {
   render() {
     return (
-      <section>
       <h1>Hello React :)</h1>
-      <SimpleMap />
-      </section>
-
-
     );
   }
 }
