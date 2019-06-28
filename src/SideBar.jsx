94f30762f7541a4acc183b160fb8c2768aa33565
MatthewYiHe <matthewhe1@hotmail.com>
2019-06-28 19:02:41 +0000

    add side bar

---
+++ src/SideBar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+class SideBar extends Component {
+  constructor(){
+    super();
+  }
+  render(){
+    return (
+      <nav className="nav">
+        <div className="plus-15">+15</div>
+        <div className="nav-item"><i className="material-icons md-48">shopping_cart</i>shopping</div>
+        <div className="nav-item"><i className="material-icons md-48">local_dining</i>reastaurants</div>
+        <div className="nav-item"><i className="material-icons md-48">account_balance</i>banking</div>
+      </nav>
+    )
+  }
+}
+export default SideBar;
