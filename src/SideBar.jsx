b87538758ce362610ad7fb4ce15235e9352ecf69
Jelena Grimshaw <muchstudio@hotmail.com>
2019-06-28 21:13:31 +0000

    toggle-switch

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -7,7 +7,15 @@ class SideBar extends Component {
     return (
       <nav className="nav">
         <div className="plus-15">+15</div>
-        <div className="nav-item"><i className="material-icons md-48">shopping_cart</i>shopping</div>
+        <div className="nav-item"><i className="material-icons md-48">shopping_cart</i>
+        shopping
+
+        <label className="switch">
+        <input type="checkbox"></input>
+        <span className="slider"></span>
+        </label>
+
+        </div>
         <div className="nav-item"><i className="material-icons md-48">local_dining</i>reastaurants</div>
         <div className="nav-item"><i className="material-icons md-48">account_balance</i>banking</div>
       </nav>
