4806143b3633c5c3ab559e20582808a794da2d51
Jelena Grimshaw <muchstudio@hotmail.com>
2019-07-01 01:13:52 +0000

    add icons to buttons

--- src/side-bar/index.jsx
+++ src/side-bar/index.jsx
@@ -9,9 +9,19 @@ function toggleSelectedCategory(category, selectedCategories, setSelectedCategor
   })
 }
 
+// function insertIcon(category) {
+//   let result = ""
+//   category.forEach(function(elm){
+//     if (elm === "coffee") return "coffeeee"
+//       else if (elm === "food") return "foooood"
+//         else if (elm === "banks") return "banksssss"
+//     })
+//   return (<p>${result}</p>)
+//   }
+// // const category = ["coffee", "food", "banks"]
 
 export default function SideBar({
-  categories, selectedCategories, setSelectedCategories
+  categories, selectedCategories, setSelectedCategories, insertIcon
 }) {
   return(
     <nav className="nav">
@@ -20,12 +30,9 @@ export default function SideBar({
       {
         categories.map(category => <li>
           <button className="nav-button"
-            onClick={() => toggleSelectedCategory(category, selectedCategories, setSelectedCategories)}
-            style={{
-              backgroundColor: selectedCategories.find(selectedCategory =>  selectedCategory === category)
-                ? '#737982' : '#9fa4ad'
-            }}
-          >{ category }</button>
+            onClick={() => toggleSelectedCategory(category, selectedCategories, setSelectedCategories, insertIcon)}
+            style={{ backgroundColor: selectedCategories.find(selectedCategory =>  selectedCategory === category) ? '#737982' : '#9fa4ad' }}
+          ><img className="button-icon" src={`/public/button-icons/${category}-button.png`} />{ category }</button>
         </li>)
       }
       </ul>
